20e23f5e50b9a51a23e80170a51fe115ecc7af89
Joël <57440945+jofmi@users.noreply.github.com>
2024-11-18 22:45:24 +0100

    feat: add layout with header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,14 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div id="app" class="flex flex-col gap-8 p-6">
+	<header>
+		<div class="shadow-box flex flex-row justify-between items-center px-7 py-6">
+			<div><h1 class="text-xl font-bold">share</h1></div>
+			<div><a href="/login">Login</a></div>
+		</div>
+	</header>
+	<div id="page" class="p-1">
+		{@render children()}
+	</div>
+</div>
