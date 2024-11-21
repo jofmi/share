f8ca84c0373b4bc6791de7692f37d3ca3ffdb949
Joël <57440945+jofmi@users.noreply.github.com>
2024-11-21 10:20:54 +0100

    feat: add app configuration and update layout header to use dynamic title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import { appConfig } from '$lib/config';
 	let { children } = $props();
 </script>
 
 <div id="app" class="flex flex-col gap-8 p-6">
 	<header>
 		<div class="shadow-box flex flex-row justify-between items-center px-7 py-6">
-			<div><h1 class="text-xl font-bold">share</h1></div>
+			<div><h1 class="text-xl font-bold">{appConfig.title}</h1></div>
 			<div><a href="/login">Login</a></div>
 		</div>
 	</header>
